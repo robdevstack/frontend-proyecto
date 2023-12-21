85b31370fe5dc8eb178aedce8ac35a80696ef1d7
roberto vera <robertodevstack@gmail.com>
2023-12-21 20:26:46 -0300

    posts unicos

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,19 +1,26 @@
 import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
+import { useAuth } from './AuthContext';
 
 const Posts = () => {
+  const { usuarioId } = useAuth();
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     // Obtiene la lista de posts desde localStorage al montar el componente
     const storedPosts = JSON.parse(localStorage.getItem('posts')) || [];
-    setPosts(storedPosts);
-  }, []);
+    // Filtra los posts por el usuarioId actual
+    const userPosts = storedPosts.filter((post) => post.usuario_id === usuarioId);
+    setPosts(userPosts);
+  }, [usuarioId]);
+
   return (
     <div>
-                    <a className="btnverde btn btn-success"><Link className="navbar-brand" to="/form">
-          volver al formulario
-        </Link></a>
+      <a className="btnverde btn btn-success">
+        <Link className="navbar-brand" to="/form">
+          Volver al formulario
+        </Link>
+      </a>
       <h2 className='titulo-posts'>Lista de Productos</h2>
       {posts.length === 0 ? (
         <p>No hay posts disponibles.</p>
