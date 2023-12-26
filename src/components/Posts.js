45235b983c414d7439d0bd4b0111f7bd39358fcc
roberto vera <robertodevstack@gmail.com>
2023-12-26 08:46:50 -0300

    posts f5

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,18 +1,62 @@
-import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 
 const Posts = () => {
   const { usuarioId } = useAuth();
+  const navigate = useNavigate();
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    // Obtiene la lista de posts desde localStorage al montar el componente
-    const storedPosts = JSON.parse(localStorage.getItem('posts')) || [];
-    // Filtra los posts por el usuarioId actual
-    const userPosts = storedPosts.filter((post) => post.usuario_id === usuarioId);
-    setPosts(userPosts);
+    const fetchUserPosts = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await fetch(`https://backend-jags.onrender.com/posts?usuario_id=${usuarioId}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setPosts(data);
+          localStorage.setItem('posts', JSON.stringify(data));
+        } else {
+          console.error('Error al obtener los posts del usuario:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error al obtener los posts del usuario:', error.message);
+      }
+    };
+
+    fetchUserPosts();
   }, [usuarioId]);
+
+  const renderPosts = () => {
+    return (
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {posts.map((post) => (
+          <div key={post.id} className="col">
+            <div className="card card1">
+              <img
+                src={post.img}
+                className="card-img-top"
+                alt={post.titulo}
+                style={{ width: '100%', height: '200px', objectFit: 'cover' }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{post.titulo}</h5>
+                <p className="card-text">${post.precio}</p>
+                <Link to={`/detalle/${post.id}`} className="btn btn-primary">
+                  Ver Más
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
   return (
     <div>
@@ -22,31 +66,7 @@ const Posts = () => {
         </Link>
       </a>
       <h2 className='titulo-posts'>Lista de Productos</h2>
-      {posts.length === 0 ? (
-        <p>No hay posts disponibles.</p>
-      ) : (
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-          {posts.map((post) => (
-            <div key={post.id} className="col">
-              <div className="card card1">
-                <img
-                  src={post.img}
-                  className="card-img-top"
-                  alt={post.titulo}
-                  style={{ width: '100%', height: '200px', objectFit: 'cover' }}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{post.titulo}</h5>
-                  <p className="card-text">${post.precio}</p>
-                  <Link to={`/detalle/${post.id}`} className="btn btn-primary">
-                    Ver Más
-                  </Link>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      )}
+      {posts.length === 0 ? <p>No hay posts disponibles.</p> : renderPosts()}
     </div>
   );
 };
