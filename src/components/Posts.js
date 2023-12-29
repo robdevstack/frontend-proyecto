c04f2e484a7e98e8137d1f9998568f6290334558
roberto vera <robertodevstack@gmail.com>
2023-12-29 15:56:24 -0300

    post ya

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -38,24 +38,33 @@ const Posts = () => {
       <div className="container d-flex justify-content-center">
         <div className="row row-cols-1 row-cols-md-3 mb-4">
           {posts.map((post) => (
-            <div className="col mb-4" key={post.id}>
-              <div className="card border-0 rounded-0 shadow" style={{ width: '18rem' }}>
+            <div key={post.id} className="col">
+              <div className="card border-0 rounded-0 shadow w-75 mx-auto">
                 <img src={post.img} className="card-img-top rounded-0" alt="..." />
                 <div className="card-body mt-3 mb-3">
-                  <h4 className="card-title">{post.titulo}</h4>
-                  <p className="card-text">
-                    <i className="bi bi-star-fill text-warning"></i>
-                    <i className="bi bi-star-fill text-warning"></i>
-                    <i className="bi bi-star-fill text-warning"></i>
-                    <i className="bi bi-star-fill text-warning"></i>
-                  </p>
+                  <div className="row">
+                    <div className="col-10">
+                      <h4 className="card-title">{post.titulo}</h4>
+                      <p className="card-text">
+                        <i className="bi bi-star-fill text-warning"></i>
+                        <i className="bi bi-star-fill text-warning"></i>
+                        <i className="bi bi-star-fill text-warning"></i>
+                        <i className="bi bi-star-fill text-warning"></i>
+                      </p>
+                    </div>
+                    <div className="col-2">
+                      <i className="bi bi-bookmark-plus fs-2"></i>
+                    </div>
+                  </div>
                 </div>
                 <div className="row align-items-center text-center g-0">
                   <div className="col-4">
-                    <h5>{post.precio}</h5>
+                    <h5>${post.precio}</h5>
                   </div>
                   <div className="col-8">
-                    <a href="#" className="btn btn-dark w-100 p-3 rounded-0 text-warning">VER MAS</a>
+                    <Link to={`/detalle/${post.id}`} className="btn btn-dark w-100 p-3 rounded-0 text-warning">
+                      VER MAS
+                    </Link>
                   </div>
                 </div>
               </div>
