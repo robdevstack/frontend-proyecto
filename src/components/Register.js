e778a774133b0429a1deba987da0b8a51b466658
roberto vera <robertodevstack@gmail.com>
2024-01-03 15:14:09 -0300

    nuevo color

--- src/components/Register.js
+++ src/components/Register.js
@@ -40,7 +40,7 @@ const Register = () => {
     };
 
     return (
-        <section class="vh-100 gradient-custom">
+        <section class="vh-100">
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
